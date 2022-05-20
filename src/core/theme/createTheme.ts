dfadf7e06daa5eb34b857e1df8eca2226f404422
Mauro E. Bender <mauro@amalgama.co>
2022-05-20 17:40:59 +0200

    Add Theme typography configuration
    - Add Typography class to handle typography related configuration.
    - Add methods to typography to map defined aliases to raw values.

--- src/core/theme/createTheme.ts
+++ src/core/theme/createTheme.ts
@@ -1,9 +1,19 @@
-import type { PaletteColorsConfig } from '../palette/types';
 import Palette from '../palette/Palette';
 import Theme from './Theme';
+import type { PaletteColorsConfig } from '../palette/types';
+import type { TypographyConfig } from '../typography/types';
+import Typography from '../typography/Typography';
 
-const createTheme = ( { palette }:{ palette: PaletteColorsConfig } ): Theme => (
-	new Theme( { palette: new Palette( palette ) } )
+type CreateThemeProps = {
+	palette: PaletteColorsConfig,
+	typography: TypographyConfig
+}
+
+const createTheme = ( { palette, typography }: CreateThemeProps ): Theme => (
+	new Theme( {
+		palette: new Palette( palette ),
+		typography: new Typography( typography )
+	} )
 );
 
 export default createTheme;
