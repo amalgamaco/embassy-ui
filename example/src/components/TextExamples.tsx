2be75fab594ddf4bcd1ff345bea63ae7bec1eb57
Mauro E. Bender <mauro@amalgama.co>
2022-05-27 19:13:00 +0200

    Update default theme and Text component examples
    - Update default theme with Amalgama's design system values.
    - Add Text component examples for all defined variants.
    - Add default theme fonts to example app.

--- example/src/components/TextExamples.tsx
+++ example/src/components/TextExamples.tsx
@@ -8,11 +8,42 @@ const styles = StyleSheet.create( {
 		flex: 1,
 		alignItems: 'flex-start',
 		justifyContent: 'flex-start'
+	},
+	separator: {
+		height: 1,
+		minWidth: '100%',
+		marginTop: 2,
+		marginBottom: 6,
+		backgroundColor: 'black'
+	},
+	vspace: {
+		height: 10,
+		minWidth: '100%'
 	}
 } );
 
 const TextExamples = () => (
 	<View style={styles.container}>
+		<Text variant="h1" bgColor="primary.200">Text Component</Text>
+		<View style={styles.vspace} />
+		<Text variant="sh1" color="primary.800">Variants</Text>
+		<View style={styles.separator} />
+		<Text variant="h1">Headline 1</Text>
+		<Text variant="h2">Headline 2</Text>
+		<Text variant="h3">Headline 3</Text>
+		<Text variant="sh1">Subtitle 1</Text>
+		<Text variant="sh2">Subtitle 2</Text>
+		<Text variant="sh3">Subtitle 3</Text>
+		<Text variant="body">Body text</Text>
+		<Text variant="body-2">Body text 2</Text>
+		<Text variant="links">Links</Text>
+		<Text variant="small">Small text</Text>
+		<Text variant="small-bold">Small bold text</Text>
+		<Text variant="caption">Caption text</Text>
+		<Text variant="caption-bold">Caption bold text</Text>
+		<View style={styles.vspace} />
+		<Text variant="sh1" color="primary.800">Free props</Text>
+		<View style={styles.separator} />
 		<Text fontSize="3xl" fontWeight="extrabold" textAlign="left" style={{ paddingBottom: 20 }}>Text</Text>
 		<Text fontSize="xs" fontStyle="italic" fontWeight="medium">Text!</Text>
 		<Text fontSize="md" color="primary.400" fontWeight="medium">Text!</Text>
