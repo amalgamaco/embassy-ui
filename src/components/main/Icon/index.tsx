ce0a80535a974d12ab556424ed99a245fa7f6ca9
Enzo Vigneti <enzo@amalgama.co>
2022-08-31 09:36:15 -0300

    Add compatibility with svg icons

--- src/components/main/Icon/index.tsx
+++ src/components/main/Icon/index.tsx
@@ -1,6 +1,15 @@
 import React, { memo, forwardRef } from 'react';
+import CustomCircle from '../../../Assets/Icons/CustomCircle';
 import { useIconPropsResolver } from './hooks';
-import type { IIconProps } from './types';
+import type { AsComponent, IIconProps } from './types';
+
+interface CUSTOM_ICONS_TYPE {
+	[key: string]: AsComponent;
+}
+
+const CUSTOM_ICONS = Object.freeze( {
+	customcircle: CustomCircle
+} ) as CUSTOM_ICONS_TYPE;
 
 const Icon = ( {
 	name, ...props
@@ -12,6 +21,18 @@ const Icon = ( {
 		style,
 		restProps
 	} = useIconPropsResolver( props );
+
+	if ( name.includes( 'custom' ) ) {
+		const Illustration = CUSTOM_ICONS[ name.toLowerCase() ];
+		return (
+			<Illustration
+				color={color}
+				size={size}
+				style={style}
+				{...restProps}
+			/>
+		);
+	}
 
 	return (
 		<BaseIconComponent
