eee3403c550cdc8e8785f001da40aa2ca616d8c9
Mauro E. Bender <mauro@amalgama.co>
2022-09-27 16:36:55 +0200

    Add missing props onChange and onChangeText to text input props list

--- src/components/main/TextInput/consts.ts
+++ src/components/main/TextInput/consts.ts
@@ -2,10 +2,10 @@
 export const TEXT_INPUT_PROP_NAMES = [
 	'allowFontScaling', 'autoCapitalize', 'autoCorrect', 'autoFocus',
 	'blurOnSubmit', 'caretHidden', 'contextMenuHidden', 'defaultValue',
-	'disabled', 'editable', 'keyboardType', 'maxLength', 'multiline', 'onBlur',
-	'onContentSizeChange', 'onEndEditing', 'onPressIn', 'onPressOut',
-	'onFocus', 'onSelectionChange', 'onSubmitEditing', 'onTextInput',
-	'onScroll', 'onKeyPress', 'placeholder', 'placeholderTextColor',
+	'disabled', 'editable', 'keyboardType', 'maxLength', 'multiline',
+	'onBlur', 'onChange', 'onChangeText', 'onContentSizeChange', 'onEndEditing',
+	'onPressIn', 'onPressOut', 'onFocus', 'onSelectionChange', 'onSubmitEditing',
+	'onTextInput', 'onScroll', 'onKeyPress', 'placeholder', 'placeholderTextColor',
 	'returnKeyType', 'secureTextEntry', 'selectTextOnFocus', 'selection',
 	'selectionColor', 'style', 'textAlign', 'inputAccessoryViewID',
 	'value', 'maxFontSizeMultiplier'
