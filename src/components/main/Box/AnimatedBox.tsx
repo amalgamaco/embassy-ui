dea0fdf338d6b37c337350036ca28e5e50f80921
Enzo Vigneti <enzo@amalgama.co>
2022-10-04 12:23:15 -0300

    Add animated box

--- src/components/main/Box/AnimatedBox.tsx
+++ src/components/main/Box/AnimatedBox.tsx
@@ -1,24 +1,24 @@
-import React, { forwardRef } from 'react';
-import { View } from 'react-native';
+import React, { forwardRef, memo } from 'react';
+import { Animated, View } from 'react-native';
 import type { ComponentProps } from '../../../core/styles/types';
 import { useComponentPropsResolver, useStyleFromPropsResolver } from '../../../hooks';
-import type { IBoxProps } from './types';
+import type { IAnimatedBoxProps } from './types';
 
-const Box = ( { children, ...props }: IBoxProps, ref: React.Ref<View> ) => {
+const AnimatedBox = ( { children, ...props }: IAnimatedBoxProps, ref: React.Ref<View> ) => {
 	const resolvedProps = useComponentPropsResolver( 'Box', props );
 	const [ style, restProps ] = useStyleFromPropsResolver(
 		'Box', resolvedProps as ComponentProps
 	);
 
 	return (
-		<View
+		<Animated.View
 			ref={ref}
 			style={style}
 			{...restProps}
 		>
-			{ children }
-		</View>
+			{children}
+		</Animated.View>
 	);
 };
 
-export default forwardRef( Box );
+export default memo( forwardRef( AnimatedBox ) );
